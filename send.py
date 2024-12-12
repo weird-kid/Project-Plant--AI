09ec2baeb44b1e2f2508f36b75e32ae35531b18c
weird-kid <109130587+weird-kid@users.noreply.github.com>
2024-12-12 06:05:59 +0530

    Update send.py

--- send.py
+++ send.py
@@ -2,40 +2,45 @@ import requests
 import os
 from .aht10_temp import *
 from .soil_moist import *
+import random
 
-
-url = "http://172.16.18.178:5000/index/"
-file_path = "/home/dhruv/PlantAi/sensors/photos/plant1.jpg"
-
-'''dabsp=os.path.abspath(file_path)
-print(dabsp)
-'''
-
-data = {
-    "plantname": "tulasi",  
-    "humidity": 65,
-    "temperature": 45,
-    "moisture": 70
-}
-
-
-data['humidity'] , data['temperature'] = main()
-data['mositure'] = Temp_humid()
-
-print('')
-print('')
-print(f'humidity->{data["humidity"]}')
-print(f'Temperature->{data["temperature"]}')
-print(f'Mositure->{data["mositure"]}')
-print('')
-print('')
-
-with open(file_path, "rb") as file:
-    files = {"image": file}
-    response = requests.post(url, data=data, files=files)
-
-print("Status Code:", response.status_code)
-try:
-    print("Response JSON:", response.json())
-except requests.exceptions.JSONDecodeError:
-    print("Response Text:", response.text)
+def send():    
+    url = "http://172.16.18.178:5000/index/"
+    file_path1 = "/home/dhruv/PlantAi/sensors/photos/plant1.jpg"
+    
+    no = random.randrange(1,10)
+    file_path2 = ".jpg"
+    file_path = file_path1 + no.strip() + file_path2
+    
+    '''dabsp=os.path.abspath(file_path)
+    print(dabsp)
+    '''
+    
+    data = {
+        "plantname": "tulasi",  
+        "humidity": 65,
+        "temperature": 45,
+        "moisture": 70
+    }
+    
+    
+    data['humidity'] , data['temperature'] = main()
+    data['mositure'] = Temp_humid()
+    
+    print('')
+    print('')
+    print(f'humidity->{data["humidity"]}')
+    print(f'Temperature->{data["temperature"]}')
+    print(f'Mositure->{data["mositure"]}')
+    print('')
+    print('')
+    
+    with open(file_path, "rb") as file:
+        files = {"image": file}
+        response = requests.post(url, data=data, files=files)
+    
+    print("Status Code:", response.status_code)
+    try:
+        print("Response JSON:", response.json())
+    except requests.exceptions.JSONDecodeError:
+        print("Response Text:", response.text)
